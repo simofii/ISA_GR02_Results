801a8e9cf9f064422688c11407a5849972cdb81f
Lore-O <lorenzoottino97@icloud.com>
2022-02-15 16:01:55 +0100

    Upload Lab03 completed.

--- Lab_03/tb/package_tb_RISC_V.vhd
+++ Lab_03/tb/package_tb_RISC_V.vhd
@@ -1,3 +1,13 @@
+-- ****************************************** --
+-- * Integrated Systems Architecture        * --
+-- * Lab 03: RISC-V                         * --  
+-- * PACKAGE for TESTBENCH top-level RISC-V * --   
+-- * Authors:                               * --
+-- *  FIORAVANTI Simone S292612             * --
+-- *  MOLINA Ottavio    S292527             * --
+-- *  OTTINO Lorenzo    S281571             * --
+-- ****************************************** --
+
 package package_tb_RISC_V is
 	constant PERIOD : time := 1 us;
 	constant Nbit : positive := 32;
